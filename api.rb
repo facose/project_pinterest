4dff81432235ad7821d692ebee8fe68567f0e0d8
Fabian Conejo <fabianconejosegu@gmail.com>
2021-02-11 21:42:21 -0600

    Duplicating Pinterest layout only using HTML and  CSS

---
+++ api.rb
@@ -0,0 +1,25 @@
+require "uri"
+require "net/http"
+require "json"
+
+
+def request(url, token = nil)
+    url = URI(url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    return JSON.parse(response.read_body)
+end 
+
+def build_web_page(info_array)
+    File.open('index.html', 'w') do |file|
+        info_array.each do |element|
+            file.puts "<img src='#{element["images"]["main"]}' alt='#{element["name"]["spanish"]}'>" 
+        end
+    end          
+end 
+
+
+response_array = request("https://aves.ninjas.cl/api/birds")
+build_web_page(response_array)
